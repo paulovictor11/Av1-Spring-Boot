417377ed0d62bad049fa4965722e094c017d9652
paulovictor11 <pauloalmeidajp652@gmail.com>
2018-03-22 09:12:25 -0300

    Primeira Parte da Avaliação

---
+++ src/main/java/com/aulapablo/springboot/SprinBootAv001Application.java
@@ -0,0 +1,12 @@
+package com.aulapablo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinBootAv001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinBootAv001Application.class, args);
+	}
+}
